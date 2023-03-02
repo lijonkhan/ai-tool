0b23262325902efc05ac3d8a13f9b67596d88eec
lijonkhan <lijonkhan76@gmail.com>
2023-03-02 22:07:59 +0600

    modify

--- layouts/partials/Footer.js
+++ layouts/partials/Footer.js
@@ -20,8 +20,7 @@ const Footer = () => {
             </li>
           ))}
         </ul>
-        {/* social icons */}
-        <Social source={social} className="social-icons mb-8" />
+
         {/* copyright */}
         {markdownify(copyright, "p", "text-light")}
       </div>
